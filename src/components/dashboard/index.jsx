66ae82a514ed8231f25c7363da86ac1b1c52076a
atulrana007 <atul.rana@zopsmart.com>
2021-11-08 21:08:24 +0530

    app for normally working

--- src/components/dashboard/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import DashboardUI from "./dashboard";
-const DashBoard = (props) => {
-    return <DashboardUI />;
-};
-export default DashBoard;
--- src/localization/messages/index.js
+++
@@ -1,8 +0,0 @@
-import en from "./en-US";
-import fr from "./fr-CA";
-
-const messages = {
-    ...en,
-    ...fr,
-};
-export default messages;
